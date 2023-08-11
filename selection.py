c172630fe65255eebcc659f15b63f3d0fc9adc11
Andrey Korabelnik <andrey.korabelnik@gmail.com>
2023-08-11 09:11:43 +0200

    continue development

--- selection.py
+++ selection.py
@@ -1,53 +1,101 @@
-import json, os
+import json
+import os
+
 import pandas as pd
+
 import general
 import sql_expr_parser
 
 
+def get_attribute_dependencies_rank(attr):
+    r = [a['attr_code'] for a in attr['rank_attrs']]
+    if 'partition_by' in attr and attr['partition_by']:
+        r.append(attr['partition_by'])
+    return r
+
+
+def get_attribute_dependencies_aggregate(attr):
+    r = [attr['aggregate_attr_code']]
+    if 'partition_by' in attr and attr['partition_by']:
+        r.append(attr['partition_by'])
+    return r
+
+
+def get_attribute_dependencies_expression(attr):
+    return sql_expr_parser.extract_identifiers(sql_expr_parser.parse(attr['expression']))
+
+
+def get_attribute_dependencies(attr_code, universe_attributes):
+    dependencies = []
+    parents = [attr_code]
+    while parents:
+        a = parents.pop()
+        if a not in dependencies:
+            attr = next(attr for attr in universe_attributes if attr['attr_code'] == a)
+            if attr['attr_type'] != 'INPUT':
+                dependencies.append(a)
+                if attr['attr_type'] == 'RANK':
+                    direct_dependencies = get_attribute_dependencies_rank(attr)
+                elif attr['attr_type'] == 'AGGREGATE':
+                    direct_dependencies = get_attribute_dependencies_aggregate(attr)
+                elif attr['attr_type'] == 'EXPRESSION':
+                    direct_dependencies = get_attribute_dependencies_expression(attr)
+                parents.extend(d for d in direct_dependencies if d not in parents)
+    return reversed(dependencies)
+
+
+def add_attribute_rank(df, attr, preceding_filters_column):
+    rank_attrs = [(a['attr_code'], a['direction'])
+                  for a in sorted(attr['rank_attrs'], key=lambda x: x['order'])]
+    if preceding_filters_column:
+        # apply preceding filters first. it ranks DESC to make rows passed filters more priority
+        rank_attrs.insert(0, (preceding_filters_column, 'DESC'))
+    combine_rank_attr = f"{attr['attr_code']}_aux"
+    df[combine_rank_attr] = df.apply(lambda row: tuple((-1 if direction == 'DESC' else 1) * row[a]
+                                                       for (a, direction) in rank_attrs), axis=1)
+    if 'partition_by' in attr and attr['partition_by']:
+        ranks = df.groupby(attr['partition_by'])[combine_rank_attr].rank(method='first')
+    else:
+        ranks = df[combine_rank_attr].rank(method='first')
+    # without reseting all values are NaN in df
+    ranks.reset_index(drop=True, inplace=True)
+    df[attr['attr_code']] = ranks
+    return df
+
+
+def add_attribute_aggregate(df, attr, preceding_filters_column):
+    aggr_attr = attr['aggregate_attr_code']
+    if preceding_filters_column:
+        # apply preceding filters first
+        df[f'{aggr_attr}_aux'] = df.apply(
+            lambda row: row[aggr_attr] if row[preceding_filters_column] else 0,
+            axis=1)
+        aggr_attr = f'{aggr_attr}_aux'
+    aggr_func = attr['aggregate_function']
+    if 'partition_by' in attr and attr['partition_by']:
+        aggrs = df.groupby(attr['partition_by'])[aggr_attr].apply(aggr_func)
+    else:
+        aggrs = df[aggr_attr].apply(aggr_func)
+    df[attr['attr_code']] = aggrs
+    return df
+
+
+def add_attribute_expression(df, attr):
+    df[attr['attr_code']] = eval(sql_expr_parser.transform_to_pandas(attr['expression']))
+    return df
+
+
 def add_attribute(df, attr_code, universe_attributes, preceding_filters_column):
-    # don't rewrite attr_code in df as it might have been added by a preceding filter
-    if attr_code not in df.columns.tolist():
-        attr = next(attr for attr in universe_attributes if attr['attr_code'] == attr_code)
-        if attr['attr_type'] == 'RANK':
-            for a in attr['rank_attrs']:
-                add_attribute(df, a['attr_code'], universe_attributes, preceding_filters_column)
-            rank_attrs = [(a['attr_code'], a['direction'])
-                          for a in sorted(attr['rank_attrs'], key=lambda x: x['order'])]
-            if preceding_filters_column:
-                # apply preceding filters first. it ranks DESC to make rows passed filters more priority
-                rank_attrs.insert(0, (preceding_filters_column, 'DESC'))
-            combine_rank_attr = f"{attr['attr_code']}_aux"
-            df[combine_rank_attr] = df.apply(lambda row: tuple((-1 if direction == 'DESC' else 1) * row[a]
-                                                               for (a, direction) in rank_attrs), axis=1)
-            if 'partition_by' in attr and attr['partition_by']:
-                add_attribute(df, attr['partition_by'], universe_attributes, preceding_filters_column)
-                ranks = df.groupby(attr['partition_by'])[combine_rank_attr].rank(method='first')
-            else:
-                ranks = df[combine_rank_attr].rank(method='first')
-            # without reseting all values are NaN in df
-            ranks.reset_index(drop=True, inplace=True)
-            df[attr['attr_code']] = ranks
-        elif attr['attr_type'] == 'AGGREGATE':
-            aggr_attr = attr['aggregate_attr_code']
-            add_attribute(df, aggr_attr, universe_attributes, preceding_filters_column)
-            if preceding_filters_column:
-                # apply preceding filters first
-                df[f'{aggr_attr}_aux'] = df.apply(
-                    lambda row: row[aggr_attr] if row[preceding_filters_column] else 0,
-                    axis=1)
-                aggr_attr = f'{aggr_attr}_aux'
-            aggr_func = attr['aggregate_function']
-            if 'partition_by' in attr and attr['partition_by']:
-                add_attribute(df, attr['partition_by'], universe_attributes, preceding_filters_column)
-                aggrs = df.groupby(attr['partition_by'])[aggr_attr].apply(aggr_func)
-            else:
-                aggrs = df[aggr_attr].apply(aggr_func)
-            df[attr['attr_code']] = aggrs
-        elif attr['attr_type'] == 'EXPRESSION':
-            # add atributes used in expression
-            for a in sql_expr_parser.extract_identifiers(sql_expr_parser.parse(attr['expression'])):
-                add_attribute(df, a, universe_attributes, preceding_filters_column)
-            df[attr['attr_code']] = eval(sql_expr_parser.transform_to_pandas(attr['expression']))
+    for a in get_attribute_dependencies(attr_code, universe_attributes):
+        # don't rewrite attr_code in df as it might have been added by a preceding filter
+        if a not in df.columns.tolist():
+            attr = next(attr for attr in universe_attributes if attr['attr_code'] == a)
+            if attr['attr_type'] == 'RANK':
+                df = add_attribute_rank(df, attr, preceding_filters_column)
+            elif attr['attr_type'] == 'AGGREGATE':
+                df = add_attribute_aggregate(df, attr, preceding_filters_column)
+            elif attr['attr_type'] == 'EXPRESSION':
+                df = add_attribute_expression(df, attr)
     return df
 
 
@@ -110,5 +158,12 @@ def run(client_input_folder, client_output_folder):
             df_out.to_csv(file, index=False, lineterminator='')
 
 
+def test():
+    with open(os.path.join('source_data', 'universe.json'), 'r') as file:
+        universe = json.load(file)
+    print(get_attribute_dependencies('LISTING_EXPRESSION', universe['attributes']))
+
+
 if __name__ == '__main__':
     run('source_data', 'output/test')
+    # test()
